74f9bacaf95b40f374a31a22ed7823a50f183cb5
atabon <willyatabong22@gmail.com>
2024-06-06 15:51:00 +0200

    validation de la creation d'un produit et la recuperation des produits enregistres

--- app/Http/Requests/SavePoductRequest.php
+++ app/Http/Requests/SavePoductRequest.php
@@ -13,7 +13,7 @@ class SavePoductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,47 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TableController;
+use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\CathegorieController;
 use App\Http\Controllers\ProduitCommandeController;
 
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('produits')
+    ->group(function () {
+        Route::controller(ProduitController::class)->group(function () {
+            Route::get('/', 'get_all_cathegory_products');
+            Route::post('/', 'save_product_by_cathegory');
+        });
+    });
+
+Route::prefix('table')->group(function () {
+    Route::controller(TableController::class)->group(function () {
+        Route::get('/', "index");
+        Route::get('/{id}', "show");
+        Route::post('/', "store");
+        Route::put('/{id}', "update");
+        Route::delete('/{id}', "destroy");
+    });
+});
+
+Route::prefix('cathegorie')->group(function () {
+    Route::controller(CathegorieController::class)->group(function () {
+        Route::get('/', "index");
+        Route::get('/{id}', "show");
+        Route::post('/', "store");
+        Route::put('/{id}', "update");
+        Route::delete('/{id}', "destroy");
+    });
+});
+
+Route::prefix('produitCommande')->group(function () {
+    Route::controller(ProduitCommandeController::class)->group(function () {
+        Route::get('/', "index");
+        Route::get('/{id}', "show");
+        Route::post('/', "store");
+        Route::put('/{id}', "update");
+        Route::delete('/{id}', "destroy");
+    });
 });
