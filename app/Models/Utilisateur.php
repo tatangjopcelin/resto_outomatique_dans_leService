b1910fb947a8a5a3cc3983ca6e9614ea0fdf9890
Jopcelin <114861813+Jopcelin@users.noreply.github.com>
2024-09-23 13:21:34 +0200

    user modified

--- app/Models/Utilisateur.php
+++ app/Models/Utilisateur.php
@@ -1,19 +1,22 @@
 <?php
-
 namespace App\Models;
 
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Utilisateur extends Model
+class Utilisateur extends Authenticatable
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
 
-    protected $fillables = [
+    protected $fillable = [
         'nom_utilisateur',
         'mot_de_pass',
         'email',
         'role',
+    ];
 
+    protected $hidden = [
+        'mot_de_pass', // Masquer le mot de passe lors de la sérialisation
     ];
 }
